fc2ba7046fe46459305666106d203c4777bb792b
sceredi <ceredi.simone.iti@gmail.com>
2024-03-14 10:19:22 +0100

    feat: created project

---
+++ app/src/main/java/a01b/e2/GUI.java
@@ -0,0 +1,37 @@
+package a01b.e2;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    private static final long serialVersionUID = -6218820567019985015L;
+    private final List<JButton> cells = new ArrayList<>();
+
+    public GUI(int size) {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(100 * size, 100 * size);
+
+        JPanel panel = new JPanel(new GridLayout(size, size));
+        this.getContentPane().add(panel);
+
+        ActionListener al = e -> {
+            var jb = (JButton) e.getSource();
+            jb.setText(String.valueOf(cells.indexOf(jb)));
+        };
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                var pos = new Pair<>(j, i);
+                final JButton jb = new JButton(pos.toString());
+                this.cells.add(jb);
+                jb.addActionListener(al);
+                panel.add(jb);
+            }
+        }
+        this.setVisible(true);
+    }
+}
